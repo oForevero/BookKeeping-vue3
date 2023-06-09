6cb6ac47f9b19ef7078595f3dc827a27541c48f2
likai <2818591096@qq.com>
2023-06-09 11:58:31 +0800

    更改sql

--- src/views/bookkeeping/purchase/BkPurchase_menu_insert.sql
+++ src/views/bookkeeping/purchase/BkPurchase_menu_insert.sql
@@ -1,26 +1,26 @@
--- 注意：该页面对应的前台目录为views/org.jeecg.bookkeeping文件夹下
+-- 注意：该页面对应的前台目录为views/bookkeeping文件夹下
 -- 如果你想更改到其他目录，请修改sql中component字段对应的值
 
 
-INSERT INTO sys_permission(id, parent_id, name, url, component, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_route, is_leaf, keep_alive, hidden, hide_tab, description, status, del_flag, rule_flag, create_by, create_time, update_by, update_time, internal_or_external) 
-VALUES ('2023052603001050400', NULL, 'bk_purchase', '/org.jeecg.bookkeeping/bkPurchaseList', 'org.jeecg.bookkeeping/BkPurchaseList', NULL, NULL, 0, NULL, '1', 0.00, 0, NULL, 1, 0, 0, 0, 0, NULL, '1', 0, 0, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0);
+INSERT INTO sys_permission(id, parent_id, name, url, component, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_route, is_leaf, keep_alive, hidden, hide_tab, description, status, del_flag, rule_flag, create_by, create_time, update_by, update_time, internal_or_external)
+VALUES ('2023052603001050400', NULL, '进货流水', '/bookkeeping/bkPurchaseList', 'bookkeeping/BkPurchaseList', NULL, NULL, 0, NULL, '1', 0.00, 0, NULL, 1, 0, 0, 0, 0, NULL, '1', 0, 0, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0);
 
 -- 权限控制sql
 -- 新增
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023052603001050401', '2023052603001050400', '添加bk_purchase', NULL, NULL, 0, NULL, NULL, 2, 'org.jeecg.bookkeeping:bk_purchase:add', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023052603001050401', '2023052603001050400', '添加进货流水记录', NULL, NULL, 0, NULL, NULL, 2, 'bookkeeping:bk_purchase:add', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
 -- 编辑
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023052603001050402', '2023052603001050400', '编辑bk_purchase', NULL, NULL, 0, NULL, NULL, 2, 'org.jeecg.bookkeeping:bk_purchase:edit', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023052603001050402', '2023052603001050400', '编辑进货流水记录', NULL, NULL, 0, NULL, NULL, 2, 'bookkeeping:bk_purchase:edit', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
 -- 删除
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023052603001050403', '2023052603001050400', '删除bk_purchase', NULL, NULL, 0, NULL, NULL, 2, 'org.jeecg.bookkeeping:bk_purchase:delete', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023052603001050403', '2023052603001050400', '删除进货流水记录', NULL, NULL, 0, NULL, NULL, 2, 'bookkeeping:bk_purchase:delete', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
 -- 批量删除
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023052603001050404', '2023052603001050400', '批量删除bk_purchase', NULL, NULL, 0, NULL, NULL, 2, 'org.jeecg.bookkeeping:bk_purchase:deleteBatch', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023052603001050404', '2023052603001050400', '批量删除进货流水记录', NULL, NULL, 0, NULL, NULL, 2, 'bookkeeping:bk_purchase:deleteBatch', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
 -- 导出excel
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023052603001050405', '2023052603001050400', '导出excel_bk_purchase', NULL, NULL, 0, NULL, NULL, 2, 'org.jeecg.bookkeeping:bk_purchase:exportXls', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023052603001050405', '2023052603001050400', '导出excel_进货流水记录', NULL, NULL, 0, NULL, NULL, 2, 'bookkeeping:bk_purchase:exportXls', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
 -- 导入excel
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
-VALUES ('2023052603001050406', '2023052603001050400', '导入excel_bk_purchase', NULL, NULL, 0, NULL, NULL, 2, 'org.jeecg.bookkeeping:bk_purchase:importExcel', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
+VALUES ('2023052603001050406', '2023052603001050400', '导入excel_进货流水记录', NULL, NULL, 0, NULL, NULL, 2, 'bookkeeping:bk_purchase:importExcel', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-05-26 15:00:40', NULL, NULL, 0, 0, '1', 0);
